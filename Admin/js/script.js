46d1764497bdcb0460a396ae49c7a58b643af606
ArafatCSE99 <yeasinarafat0408@gmail.com>
2024-03-12 19:24:01 +0600

    Login Added

---
+++ Admin/js/script.js
@@ -0,0 +1,31 @@
+function checkCredential()
+{
+    var username=$('#username').val();
+    var password=$('#password').val();
+
+    if(username=='' || password=='')
+    {
+        alert("Please Insert UserName & Password!");
+    }
+    else{
+    
+        var dataString="username="+username+"&password="+password;
+
+        $.ajax({
+            type: "POST",
+            url: "API/checkCredential.php",
+            data: dataString,
+            cache: false,
+            success: function(html) {
+               if(html=="1"){
+                window.location.href = "Home.php";
+               }
+               else{
+                alert("Wrong Username or Password!");
+               }
+               
+            }
+            });
+
+    }
+}
